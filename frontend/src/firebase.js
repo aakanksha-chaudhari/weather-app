d9a718aeb25cda114c9b31ad3ca4d1122952a69a
Aakanksha Chaudhari <aakankshac773@gmail.com>
2025-11-02 13:06:51 +0530

    Initial commit

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBiWXPp3bvKj8fBLgmcluE-x4PwLgMUmAk",
   authDomain: "weatherapp-26dab.firebaseapp.com",
   projectId: "weatherapp-26dab",
-  storageBucket: "weatherapp-26dab.appspot.com",
+   storageBucket: "weatherapp-26dab.appspot.com",
   messagingSenderId: "785366764870",
   appId: "1:785366764870:web:af389fe263553911e4a947",
   measurementId: "G-KTSZ4SB1DW",
